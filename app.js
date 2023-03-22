50a63d1b0a1ca16b37a4a964a195ece94360f0fd
developerMohib <92154638+developerMohib@users.noreply.github.com>
2023-03-23 00:27:25 +0600

    all added done

---
+++ app.js
@@ -0,0 +1,27 @@
+function myFunction() {
+    var x = document.getElementById("navDemo");
+    if (x.className.indexOf("w3-show") == -1) {
+      x.className += " w3-show";
+    } else { 
+      x.className = x.className.replace(" w3-show", "");
+    }
+  }
+
+
+  
+  const scrollToTopBtn = document.getElementById("scroll-to-top");
+
+  window.addEventListener("scroll", () => {
+    if (document.documentElement.scrollTop > 100) {
+      scrollToTopBtn.style.display = "block";
+    } else {
+      scrollToTopBtn.style.display = "none";
+    }
+  });
+
+  scrollToTopBtn.addEventListener("click", () => {
+    document.documentElement.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  });
